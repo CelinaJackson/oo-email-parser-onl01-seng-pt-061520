2e1345dab06e2fc5c3a88cbeef2d7d02029682ed
Celina Jackson <celinanjackson@gmail.com>
2020-07-11 23:00:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,8 @@
 class EmailAddressParser
 
-  attr_accessor :email
+  attr_accessor :name, :csv_emails
 
-  def self.new_from_csv(csv_data)
+  def self.new_from_csv(csv_emails)
+    @csv_emails = csv_emails
   end
 end
